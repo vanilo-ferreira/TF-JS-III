5dec354f3e7493090a660785233ccd44b52cdfb3
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-04-15 23:04:40 -0300

    feat: Adicionar resolução do exercício 03

--- aula01/exercicios/aula/exercicio-03.js
+++ aula01/exercicios/aula/exercicio-03.js
@@ -7,3 +7,25 @@ let carros = [
     { modelo: "Chevrolet Cruze", ano: 2019, precoDia: 85, disponivel: true },
     { modelo: "Volkswagen Golf", ano: 2020, precoDia: 95, disponivel: true }
 ];
+
+let carrosDisponiveis = [];
+let carrosIndisponiveis = [];
+
+for (let carro in carros) {
+    if (carros[carro].disponivel === true) {
+        carrosDisponiveis.push({ modelo: carros[carro].modelo, total: (carros[carro].precoDia * 3) });
+    } else {
+        carrosIndisponiveis.push({ modelo: carros[carro].modelo, total: (carros[carro].precoDia * 3) });
+    }
+};
+console.log("Valor da locação dos carros por 3 dias\n")
+
+console.log("Carros DISPONÍVEIS: ")
+for (let i = 0; i < carrosDisponiveis.length; i++) {
+    console.log(`${i+1}) ${carrosDisponiveis[i].modelo} - R$ ${carrosDisponiveis[i].total}`);
+}
+
+console.log("\nCarros INDISPONÍVEIS no momento: ")
+for (let i = 0; i < carrosIndisponiveis.length; i++) {
+    console.log(`${i+1}) ${carrosIndisponiveis[i].modelo} - R$ ${carrosIndisponiveis[i].total}`);
+}
